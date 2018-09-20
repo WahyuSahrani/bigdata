5b94cf6075019bbf2f0d1572d14b889e82b4003f
wahyusahrani <38240019+WahyuSahrani@users.noreply.github.com>
2018-09-20 17:20:41 +0700

    Update list.py

--- minggu-03/praktik/src/list.py
+++ minggu-03/praktik/src/list.py
@@ -1,1 +1,9 @@
-
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits.count('apple')'
+fruits.count('tangerine')
+fruits.index('banana')
+fruits.index('banana', 4)
+fruits.reverse()
+fruits.append('grape')
+fruits.sort()
+fruits.pop()
